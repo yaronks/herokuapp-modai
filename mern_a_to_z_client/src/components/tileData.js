92d3207a3ab8a8306bc2524f892ce5bcc95917ac
Daniella <danigrim@stanford.edu>
2023-02-27 17:12:09 -0800

    fix closet images

--- mern_a_to_z_client/src/components/tileData.js
+++ mern_a_to_z_client/src/components/tileData.js
@@ -38,24 +38,24 @@ const tileData = [
       album: 'Office Wear',
     },
     {
-      img: 'https://images.pexels.com/photos/1394499/pexels-photo-1394499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      title: 'Reema English',
+      img: 'https://img.ltwebstatic.com/images3_pi/2021/10/22/1634870583399e097fd1feb7de8a42c5bd592ac215.webp',
+    title: 'Knitted Sweater',
     },
     {
-      img: 'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=f05c14dd4db49f08a789e6449604c490',
-      title: 'Atticus Aldred',
+      img: 'https://img.ltwebstatic.com/images3_pi/2021/10/22/1634870583399e097fd1feb7de8a42c5bd592ac215.webp',
+    title: 'Knitted Sweater',
     },
     {
-      img: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-      title: 'Cobie Morin',
+      img: 'https://img.ltwebstatic.com/images3_pi/2021/10/22/1634870583399e097fd1feb7de8a42c5bd592ac215.webp',
+    title: 'Knitted Sweater',
     },
     {
-      img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350',
-      title: 'Corinne Mcghee',
+      img: 'https://img.ltwebstatic.com/images3_pi/2021/10/22/1634870583399e097fd1feb7de8a42c5bd592ac215.webp',
+      title: 'Knitted Sweater',
     },
     {
-      img: 'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&h=350',
-      title: 'Ali Hendricks',
+      img: 'https://img.ltwebstatic.com/images3_pi/2021/10/22/1634870583399e097fd1feb7de8a42c5bd592ac215.webp',
+      title: 'Knitted Sweater',
     },
   ];
   
